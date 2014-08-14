f0595b2dffa8a0ff92f189a38394e49f94d808d8
Hugo Ledoux <h.ledoux@tudelft.nl>
2014-08-14 17:40:45 +0200

    script to create the zillions of SHP based on attributes

--- python/hem.py
+++ python/hem.py
@@ -5,12 +5,12 @@ import sys
 import os
 import glob
 import fiona
+import shutil
 import subprocess
 from shapely.geometry import Polygon
 from shapely.geometry import Point
 from shapely.geometry import asShape
 from shapely.geometry import mapping
-# from shapely.ops import cascaded_union
 from shapely.ops import unary_union
 
 #-- Global variables
@@ -25,44 +25,62 @@ def main():
         lsFiles.append(fIn)
 
     # 1. validate each PP first
-    # for f in lsFiles:
-    #     validatepp(f)
+    valid = True
+    for f in lsFiles:
+        if (validatepp(f) == False):
+            valid = False
+    if valid == False:
+        sys.exit()
 
     # 2. hierarchical validation
-    for i in range(3,1,-1):
+    for i in range(6, 1, -1):
         j = i - 1
-
-        c = fiona.open(lsFiles[i-1], 'r')
+        ci = fiona.open(lsFiles[i-1], 'r')
         dSHN = {}
-        for f in c:
+        for f in ci:
             code = f['properties']['SHN'][:(j*2)]
             if code not in dSHN:
                 dSHN[code] = []
-            dSHN[code].append(asShape(f['geometry']))
-        print len(dSHN.keys())
-        print dSHN.keys()
+            dSHN[code].append(f)
+        # print len(dSHN)
+        cj = fiona.open(lsFiles[j-1], 'r')
+        for key in dSHN:
+            print key
+            # create folder
+            fname = "%s_%s" % (lsFiles[i-1][:-4], key)
+            if os.path.exists(fname):
+                shutil.rmtree(fname)
+            os.mkdir(fname)
+            # write the subset of the polygons
+            name = "%s/%s.shp" % (fname, key)
+            with fiona.open(name, 
+                            'w',
+                            driver=ci.driver,
+                            crs=ci.crs,
+                            schema=ci.schema) as output:
+                for f in dSHN[key]:
+                    output.write(f)
+            output.close()
 
-
-
-
-
-
-        sys.exit()
-        # polys.append(asShape(f['geometry']))
-        union = unary_union(polys)
-        if union.geom_type != 'Polygon' or union.is_valid == False:
-            print "ERROR: spatial extent not a single valid polygon. Abort."
-            sys.exit()
-
-        with fiona.open('out.shp', 
-                        'w',
-                        driver=c.driver,
-                        crs=c.crs,
-                        schema=c.schema) as output:
-            f['geometry'] = mapping(union) 
-            output.write(f)
-
-
+            polygons = []
+            for f in dSHN[key]:
+                polygons.append(asShape(f['geometry']))
+            if len(polygons) == 0:
+                print "ERROR: no matching parent. Abort"
+                sys.exit()
+            union = unary_union(polygons)
+            if union.is_valid == False:
+                print "ERROR: spatial extent not a valid polygon. Abort."
+                sys.exit()
+            # print union.geom_type, "\n"
+            name = "%s/%s_extent.shp" % (fname, key)
+            with fiona.open(name, 
+                            'w',
+                            driver=cj.driver,
+                            crs=cj.crs,
+                            schema=cj.schema) as output:
+                f['geometry'] = mapping(union) 
+                output.write(f)
 
 
 
@@ -80,34 +98,11 @@ def validatepp(fIn):
     if o.find('NOT valid.') != -1:
         print "ERROR: pp is invalid"
         print o
+        return False
     else:
         print "\tvalid."
+        return True
 
 
-def do(infile):
-    c = fiona.open(infile, 'r')
-    #-- Find and print the number of geometries
-    print "Number of features:", len(c)
-    #-- store the geometries in a list
-    lsLines = []
-    for each in c:
-        lsLines.append(asShape(each['geometry']))
-
-    querypt = Point(79152, 445909)
-    dist = 1e9
-    closest = 0
-    for i,l in enumerate(lsLines):
-        d = l.distance(querypt)
-        if d < dist:
-            dist = d
-            closest = i
-    print "distance:", dist
-    print "ID", closest
-    print lsLines[closest]
-
-    print "done."
-
-    
-      
 if __name__ == "__main__":
     main()
